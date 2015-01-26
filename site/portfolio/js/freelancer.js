f1af1d6af429d903d35bd4f918b823018e8f04a3
Sumeet Singh <sumeet_singh871@yahoo.com>
2015-01-26 22:09:42 +0530

    Initial CheckIN

---
+++ site/portfolio/js/freelancer.js
@@ -0,0 +1,55 @@
+/*!
+ * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('body').on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Floating label headings for the contact form
+$(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+function submitMailTo() {
+    var a = $('<a/>');
+    var subject = 'Message From '+encodeURI($('#name').val());
+    var valid = false;
+    $('.floating-label-form-group').each(function(ind,ele) {
+        valid = $(ele).hasClass('floating-label-form-group-with-value');
+    });
+    if(!valid) {
+        console.log('returning');
+        return false;
+    }
+    var emailFrom = $('#email').val();
+    var message = encodeURI($('#message').val());
+    var href = 'mailto:sumeet_singh871@yahoo.com?subject='+encodeURI(subject)+'&body='+message+'&cc='+emailFrom;
+    window.location.href= href;
+    return false;
+}
